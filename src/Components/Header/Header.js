8d22b588fa45d766291311b7d35ee5e8c17350c0
Wanessa Guedes <wanessa.o.guedes@gmail.com>
2022-05-11 18:52:50 -0300

    feat: Creating page header

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,31 @@
+
+import { ContainerHeader, ImgHeader, Background, TitleHeader, IconsHeader, IconDisplayCart, IconDisplayUser, IconDisplayExit } from "./styled"
+
+export default function Header(){
+    return (
+        <ContainerHeader>
+            <ImgHeader>
+                <Background>
+                            <TitleHeader>
+                                <h1>Nova</h1>
+                            </TitleHeader>
+                        <IconsHeader>
+                            <IconDisplayCart aria-haspopup="true">
+                                <span><ion-icon name="cart-outline"></ion-icon></span>
+                                <p>Carrinho</p>
+                            </IconDisplayCart>
+                            <IconDisplayUser aria-haspopup="true">
+                                <span ><ion-icon name="person-outline"></ion-icon></span>
+                                <p>Login /
+                                    Cadastro</p>
+                            </IconDisplayUser>
+                            <IconDisplayExit aria-haspopup="true">
+                                <span><ion-icon name="exit-outline"></ion-icon></span>
+                                <p>Encerrar sessão</p>
+                            </IconDisplayExit>
+                        </IconsHeader>
+                </Background>
+            </ImgHeader>
+        </ContainerHeader>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReacDom from "react-dom";
+
+import App from "./Components/App";
+import "./../src/Assets/css/reset.css";
+import "./../src/Assets/css/style.css";
+
+ReacDom.render(<App />, document.querySelector(".root"));
