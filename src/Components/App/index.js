5e009d36aa06bfb055a0a44232ddb0bc0b7100b2
Wanessa Guedes <wanessa.o.guedes@gmail.com>
2022-05-13 14:35:44 -0300

    feat: starting confirmation Page back

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Header from "../Header/Header";
 import SignUp from "../SignUp/SignUp";
+import ConfirmantionPage from "../ConfirmationScreen/Confirmation";
 
 export default function App() {
     return (
@@ -9,6 +10,7 @@ export default function App() {
             <Header/>
                 <Routes>
                     <Route path="/signup" element={<SignUp />}></Route> 
+                    <Route path="/confirmation" element={<ConfirmantionPage />}></Route>
                 </Routes>
         </BrowserRouter>
     )
